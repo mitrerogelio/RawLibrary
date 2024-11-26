1fe43c2cd8666f98893b63fa9aaa706f3667d351
rogelio <rogelinmitre@gmail.com>
2024-11-26 07:42:19 -0600

    second commit

---
+++ Model/Book.cs
@@ -0,0 +1,11 @@
+namespace RawLibrary.Model;
+
+public class Book
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public string ISBN { get; set; }
+    public string Genre { get; set; }
+}
